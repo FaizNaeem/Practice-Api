e109b30e1fa5da38b9cb04659a116aded3f2ee7b
FaizNaeem <faiznaeem192022@gmail.com>
2023-09-15 17:37:06 +0500

    object.value and object.entires

--- src/com/Api.js
+++ src/com/Api.js
@@ -5,16 +5,7 @@ export default   function Api() {
     let auth=async()=>{
         let data = await fetch('https://dummyjson.com/products/1')
         Setuser(await data.json())
-        // Setuser(new1)
-        // console.log(user);
-//         new1.map((e)=>{
-// console.log(e);
-//         })
-// console.log(new1);
-// Setuser(new1)
-//         user.map((e,i,a)=>{
-// console.log(e,i,a);
-        // })
+ 
     }
    useEffect(()=>{
 auth()
